883cebd8e4e0d88238558d438f02a86e70fe16a0
Ifeanyi Favour <ifeanyifavour041@gmail.com>
2026-04-20 10:46:02 +0100

    new file:   sumbit.php

---
+++ sumbit.php
@@ -0,0 +1,10 @@
+<?php
+$name = $_POST['full-name'];
+$email = $_POST['business-email'];
+$company = $_POST['company-name'];
+$size = $_POST['company-size'];
+$phone = $_POST['phone-number'];
+$message = $_POST['message'];
+
+echo "Form received: $name - $email";
+?>
