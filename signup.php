163d0e00d4264a3674da8d9ebc37b6bc7f885fb0
Ifeanyi Favour <ifeanyifavour041@gmail.com>
2026-05-15 18:06:52 +0100

    modified

--- signup.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$name = $_POST['fullname'];
-$email = $_POST['email'];
-$company = $_POST['com_name'];
-$password = $_POST['pass'];
-$comPass = $_POST['con_pass'];
-echo "Form received: $name - $email";
-?>
